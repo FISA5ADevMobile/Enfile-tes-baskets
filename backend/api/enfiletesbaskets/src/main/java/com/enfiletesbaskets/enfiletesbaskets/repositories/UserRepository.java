b44cc135f1065c0c62bbaecafac2605f7f1d8aae
Remy Piazza <remypiazza@Ordinateur-portable-de-Remy.local>
2024-12-05 12:14:27 +0100

    Fix: Creation table

--- backend/api/enfiletesbaskets/src/main/java/com/enfiletesbaskets/enfiletesbaskets/repositories/UserRepository.java
+++ backend/api/enfiletesbaskets/src/main/java/com/enfiletesbaskets/enfiletesbaskets/repositories/UserRepository.java
@@ -2,16 +2,16 @@ package com.enfiletesbaskets.enfiletesbaskets.repositories;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.enfiletesbaskets.enfiletesbaskets.models.UserModel;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserModel, Long> {
+public interface UserRepository extends JpaRepository<UserModel, Long> {
 
     @Modifying
     @Query(value = "INSERT INTO User_Courses (user_id, course_id) VALUES (:userId, :courseId)", nativeQuery = true)
