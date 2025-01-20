410d9b7d5fd0ceeb450537ab21b1d55954d7453b
meriem2801 <meriem.dogheche@gmail.com>
2025-01-20 16:02:35 +0100

    fix : Utilisation des coloris prédéfinis en amont

--- frontend/mobile/enfiletesbasket/lib/screens/classes_page.dart
+++ frontend/mobile/enfiletesbasket/lib/screens/classes_page.dart
@@ -47,8 +47,9 @@ class ClassesPage extends StatelessWidget {
         },
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color(0xFF0081A1),
         onPressed: () => _showJoinCourseDialog(context),
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.add, color : Colors.white),
       ),
     );
   }
@@ -63,44 +64,77 @@ class ClassesPage extends StatelessWidget {
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: const Text('Rejoindre un parcours'),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          title: const Text(
+            'Rejoindre un parcours',
+            style: TextStyle(color: Color(0xFF0081A1)),
+          ),
           content: TextField(
             onChanged: (value) => classPassword = value,
             obscureText: true,
             decoration: const InputDecoration(labelText: 'Mot de passe du parcours'),
           ),
           actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text('Annuler'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                final responseMessage = await courseProvider.subscribeToCourseWithPassword(
-                  classPassword,
-                  token,
-                );
-
-                Navigator.pop(context);
-                showDialog(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    title: const Text('Inscription à un parcours'),
-                    content: Text(responseMessage),
-                    actions: [
-                      TextButton(
-                        onPressed: () => Navigator.pop(context),
-                        child: const Text('OK'),
-                      ),
-                    ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text(
+                    'Annuler',
+                    style: TextStyle(color: Color(0xFF0081A1)),
                   ),
-                );
-              },
-              child: const Text('Rejoindre'),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF0081A1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                  onPressed: () async {
+                    final responseMessage = await courseProvider.subscribeToCourseWithPassword(
+                      classPassword,
+                      token,
+                    );
+
+                    Navigator.pop(context);
+                    showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                        title: const Text(
+                          'Inscription à un parcours',
+                          style: TextStyle(color: Color(0xFF0081A1)),
+                        ),
+                        content: Text(responseMessage),
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: const Text(
+                              'OK',
+                              style: TextStyle(color: Color(0xFF0081A1)),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    'Rejoindre',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
             ),
           ],
         );
       },
     );
   }
+
 }
