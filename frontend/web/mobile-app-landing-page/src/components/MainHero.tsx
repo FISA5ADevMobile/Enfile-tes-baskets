c882800cf838ae6283c4a15a83fd84ede5d77468
mayel15 <papemayel1515@gmail.com>
2025-02-02 20:40:54 +0100

    fix: rel for target blank

--- frontend/web/mobile-app-landing-page/src/components/MainHero.tsx
+++ frontend/web/mobile-app-landing-page/src/components/MainHero.tsx
@@ -34,6 +34,7 @@ const MainHero = () => {
                 }
               }}
               target="_blank"
+              rel="noopener noreferrer"
             >
               {mainHero.primaryAction.text}
             </a>
@@ -48,6 +49,7 @@ const MainHero = () => {
                 }
               }}
               target="_blank"
+              rel="noopener noreferrer"
               className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md border-primary text-primary bg-background hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
             >
               {mainHero.secondaryAction.text}
